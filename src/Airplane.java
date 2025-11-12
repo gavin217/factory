aaf516bbf3e52798ccf6a1514771df637db3627f
gavin217 <gavin_mcbrine28@milton.edu>
2025-11-12 09:05:42 -0500

    added airplane

---
+++ src/Airplane.java
@@ -0,0 +1,19 @@
+public class Airplane {
+    ///  instance variables
+    /// used in whole class
+    /// they should be at the top of the class
+    /// need public in front of them
+    public int capacity;
+    public String name;
+    public String model;
+    public boolean isDoubleDecker;
+    public int length;
+    public Airplane(){
+        capacity=250;
+        name="Jacob6";
+        model="boeing 737";
+        isDoubleDecker=false;
+        length=157;
+
+    }
+}
---
+++ src/Factory.java
@@ -0,0 +1,51 @@
+public class Factory {
+    int yearFounded=1798;
+    public String factoryName = "airplane factory";
+    public boolean isOpen=true;
+
+    public static void main(String[] args) {
+        System.out.println("welcome to Gavin's factory");
+        new Factory();
+    }
+
+    //constructor
+    //is a method
+    //has same name as the class
+    //public but no void
+    public Factory(){
+        isOpen=false;
+        String manager="Mr Hales";
+        System.out.println(manager);
+        //factoryInfo();
+        yearFounded=1800;
+        factoryInfo();
+        //type varname=value
+        //object of the type airplane
+        Airplane plane1=new Airplane();
+        plane1.name="jeff";
+        System.out.println(plane1.name);
+        plane1.capacity=360;
+        plane1.model="airbus";
+        plane1.isDoubleDecker=true;
+        plane1.length=70;
+        System.out.println("the plane can fit "+plane1.capacity+" people");
+        System.out.println("it is an "+plane1.model);
+        System.out.println("it is "+plane1.isDoubleDecker+" that the plane has a double decker");
+        System.out.println("it is "+plane1.length+" m long");
+        Airplane plane2=new Airplane();
+        System.out.println(plane2.capacity);
+        //todo make 2 more airplane objects
+        //todo for one change all instane variables
+        //todo for both print all instance variables
+
+
+    }
+
+    public void factoryInfo(){
+
+        System.out.println("founded in "+ yearFounded);
+        System.out.println(factoryName);
+        System.out.println("it is "+isOpen +" that the factory is open");
+
+    }
+}
