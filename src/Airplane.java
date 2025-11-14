28e1a62a878ab8334823ec5007a614b4687bd214
gavin217 <gavin_mcbrine28@milton.edu>
2025-11-14 14:57:09 -0500

    added airplane constructor and a lazy way to write the variables of the planes

--- src/Airplane.java
+++ src/Airplane.java
@@ -8,12 +8,30 @@ public class Airplane {
     public String model;
     public boolean isDoubleDecker;
     public int length;
-    public Airplane(){
+
+
+    public Airplane(){//constructor
         capacity=250;
         name="Jacob6";
         model="boeing 737";
         isDoubleDecker=false;
         length=157;
 
+    }
+    public Airplane(int pCapacity){
+        capacity= pCapacity;
+        name="Jacob6";
+        model="boeing 737";
+        isDoubleDecker=false;
+        length=157;
+
+    }
+    //todo make a method called printinfo that prints the airplanes instance variables
+    public void Printinfo(){
+        System.out.println(capacity);
+        System.out.println(model);
+        System.out.println(name);
+        System.out.println(length);
+        System.out.println(isDoubleDecker);
     }
 }
--- src/Factory.java
+++ src/Factory.java
@@ -19,40 +19,13 @@ public class Factory {
         //factoryInfo();
         yearFounded=1800;
         factoryInfo();
+        Airplane plane3=new Airplane(232);
+        System.out.println(plane3.capacity);
+        plane3.Printinfo();
         //type varname=value
         //object of the type airplane
-        Airplane plane1=new Airplane();
-        plane1.name="jeff";
-        System.out.println(plane1.name);
-        plane1.capacity=360;
-        plane1.model="airbus";
-        plane1.isDoubleDecker=true;
-        plane1.length=70;
-        System.out.println("the plane can fit "+plane1.capacity+" people");
-        System.out.println("it is an "+plane1.model);
-        System.out.println("it is "+plane1.isDoubleDecker+" that the plane has a double decker");
-        System.out.println("it is "+plane1.length+" m long");
-        Airplane plane2=new Airplane();
-        System.out.println(plane2.capacity);
-        System.out.println(plane2.name);
-        System.out.println(plane2.model);
-        System.out.println(plane2.isDoubleDecker);
-        System.out.println(plane2.length);
-        Airplane plane3=new Airplane();
-        plane1.name="jeff";
-        System.out.println(plane1.name);
-        plane1.capacity=360;
-        plane1.model="airbus";
-        plane1.isDoubleDecker=true;
-        plane1.length=70;
-        System.out.println(plane3.capacity);
-        System.out.println(plane3.name);
-        System.out.println(plane3.model);
-        System.out.println(plane3.isDoubleDecker);
-        System.out.println(plane3.length);
-        //todo make 2 more airplane objects
-        //todo for one change all instane variables
-        //todo for both print all instance variables
+
+
 
 
     }
