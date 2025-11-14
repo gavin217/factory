46c7673e241dbbbd695d06adafe622b643adecaf
gavin217 <gavin_mcbrine28@milton.edu>
2025-11-14 11:36:19 -0500

    added 2 airplanes

--- src/Factory.java
+++ src/Factory.java
@@ -34,6 +34,22 @@ public class Factory {
         System.out.println("it is "+plane1.length+" m long");
         Airplane plane2=new Airplane();
         System.out.println(plane2.capacity);
+        System.out.println(plane2.name);
+        System.out.println(plane2.model);
+        System.out.println(plane2.isDoubleDecker);
+        System.out.println(plane2.length);
+        Airplane plane3=new Airplane();
+        plane1.name="jeff";
+        System.out.println(plane1.name);
+        plane1.capacity=360;
+        plane1.model="airbus";
+        plane1.isDoubleDecker=true;
+        plane1.length=70;
+        System.out.println(plane3.capacity);
+        System.out.println(plane3.name);
+        System.out.println(plane3.model);
+        System.out.println(plane3.isDoubleDecker);
+        System.out.println(plane3.length);
         //todo make 2 more airplane objects
         //todo for one change all instane variables
         //todo for both print all instance variables
